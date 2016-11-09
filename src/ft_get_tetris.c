efbab5ee0d17b99e0ccdb7f5eee7c16adb2d4fc0
Jerome Guyon <jguyon@student.42.fr>
2016-11-09 21:30:07 +0100

    Norm

--- src/ft_get_tetris.c
+++ src/ft_get_tetris.c
@@ -6,21 +6,18 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 19:15:09 by jguyon            #+#    #+#             */
-/*   Updated: 2016/11/09 20:45:40 by jguyon           ###   ########.fr       */
+/*   Updated: 2016/11/09 21:29:28 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fillit.h"
 
-static t_tetri	*ft_get_tetri(char **content)
+static void		ft_read_tetri(char **content, t_tetri *tetri)
 {
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	t_tetri	*tetri;
 
-	if (!(tetri = (t_tetri *)malloc(sizeof(*tetri))))
-		return (NULL);
 	k = 0;
 	i = 0;
 	while (i < BLOCK_COUNT)
@@ -41,42 +38,33 @@ static t_tetri	*ft_get_tetri(char **content)
 		++i;
 	}
 	++(*content);
-	return (tetri);
 }
 
 static void		ft_normalize_tetri(t_tetri *tetri)
 {
-	size_t	xmin;
-	size_t	ymin;
-	size_t	xmax;
-	size_t	ymax;
-	size_t	i;
+	t_block	min;
+	t_block	max;
+	int		i;
 
-	xmin = BLOCK_COUNT - 1;
-	ymin = BLOCK_COUNT - 1;
-	xmax = 0;
-	ymax = 0;
-	i = 0;
-	while (i < BLOCK_COUNT)
+	min.x = BLOCK_COUNT - 1;
+	min.y = BLOCK_COUNT - 1;
+	max.x = 0;
+	max.y = 0;
+	i = -1;
+	while (++i < BLOCK_COUNT)
 	{
-		if (tetri->blocks[i].x < xmin)
-			xmin = tetri->blocks[i].x;
-		if (tetri->blocks[i].y < ymin)
-			ymin = tetri->blocks[i].y;
-		if (tetri->blocks[i].x > xmax)
-			xmax = tetri->blocks[i].x;
-		if (tetri->blocks[i].y > ymax)
-			ymax = tetri->blocks[i].y;
-		++i;
+		min.x = tetri->blocks[i].x < min.x ? tetri->blocks[i].x : min.x;
+		min.y = tetri->blocks[i].y < min.y ? tetri->blocks[i].y : min.y;
+		max.x = tetri->blocks[i].x > max.x ? tetri->blocks[i].x : max.x;
+		max.y = tetri->blocks[i].y > max.y ? tetri->blocks[i].y : max.y;
 	}
-	tetri->width = xmax - xmin + 1;
-	tetri->height = ymax - ymin + 1;
-	i = 0;
-	while (i < BLOCK_COUNT)
+	tetri->width = max.x - min.x + 1;
+	tetri->height = max.y - min.y + 1;
+	i = -1;
+	while (++i < BLOCK_COUNT)
 	{
-		tetri->blocks[i].x -= xmin;
-		tetri->blocks[i].y -= ymin;
-		++i;
+		tetri->blocks[i].x -= min.x;
+		tetri->blocks[i].y -= min.y;
 	}
 }
 
@@ -87,8 +75,9 @@ size_t			ft_get_tetris(char *content, size_t count, t_tetri **tetris)
 	i = 0;
 	while (i < count)
 	{
-		if (!(tetris[i] = ft_get_tetri(&content)))
+		if (!(tetris[i] = (t_tetri *)malloc(sizeof(**tetris))))
 			return (0);
+		ft_read_tetri(&content, tetris[i]);
 		ft_normalize_tetri(tetris[i++]);
 	}
 	return (i);
