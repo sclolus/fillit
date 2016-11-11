176f602fb975ac84fdd34fd21ca4badac1d4c4a3
Sebastien CLOLUS <sclolus@e3r10p21.42.fr>
2016-11-11 21:56:28 +0100

    Norme de ft_claim.c

--- src/ft_claim.c
+++ src/ft_claim.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 04:33:30 by jguyon            #+#    #+#             */
-/*   Updated: 2016/11/11 09:38:06 by jguyon           ###   ########.fr       */
+/*   Updated: 2016/11/11 21:55:41 by sclolus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ int		ft_claim(t_square *square, t_tetri *tetri)
 
 	shifter = 28 - tetri->x;
 	if ((square->grid[tetri->y] & (((unsigned int)tetri->box.row0) << shifter))
-			|| (square->grid[tetri->y + 1] & (((unsigned int)tetri->box.row1) << shifter))
-			|| (square->grid[tetri->y + 2] & (((unsigned int)tetri->box.row2) << shifter))
-			|| (square->grid[tetri->y + 3] & (((unsigned int)tetri->box.row3) << shifter)))
+			|| (square->grid[tetri->y + 1] 
+			& (((unsigned int)tetri->box.row1) << shifter))
+			|| (square->grid[tetri->y + 2]
+			& (((unsigned int)tetri->box.row2) << shifter))
+			|| (square->grid[tetri->y + 3]
+			& (((unsigned int)tetri->box.row3) << shifter)))
 		return (0);
 	square->grid[tetri->y] ^= ((unsigned int)tetri->box.row0) << shifter;
 	square->grid[tetri->y + 1] ^= ((unsigned int)tetri->box.row1) << shifter;
